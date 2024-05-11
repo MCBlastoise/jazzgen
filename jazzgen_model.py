363f3f6da2bc50d2a27f0d92f42a491138dff1ac
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2024-05-11 16:58:39 -0400

    Made model dimensions optionally configurable

--- jazzgen_model.py
+++ jazzgen_model.py
@@ -2,13 +2,21 @@ import torch
 import torch.nn as nn
 
 class JazzGenModel(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+    EMBEDDING_DIM = 6
+    HIDDEN_DIM = 6
+    
+    def __init__(self, vocab_size, tagset_size, embedding_dim=None, hidden_dim=None):
         super(JazzGenModel, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.music_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=False)
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        if embedding_dim is not None:
+            self.EMBEDDING_DIM = embedding_dim
+        if hidden_dim is not None:
+            self.HIDDEN_DIM = hidden_dim
+
+        self.music_embeddings = nn.Embedding(vocab_size, self.EMBEDDING_DIM)
+
+        self.lstm = nn.LSTM(self.EMBEDDING_DIM, self.HIDDEN_DIM, batch_first=False)
+        self.hidden2tag = nn.Linear(self.HIDDEN_DIM, tagset_size)
     
     def forward(self, music_in):
         embeds = self.music_embeddings(music_in)
